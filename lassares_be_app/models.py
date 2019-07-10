b15803cf253f5ae79f4f495a92db9341b1584f54
Jim McManus <jmpmcmanus@flatapps.net>
2019-07-10 10:20:47 -0400

    Added variables to model

--- lassares_be_app/models.py
+++ lassares_be_app/models.py
@@ -4,12 +4,30 @@ from django.contrib.gis.db import models
 # Create your models here.
 class fdr_18001_0_11_Model(models.Model):
     ogc_fid = models.IntegerField(primary_key=True)
-    wkb_geometry = models.MultiLineStringField()
+    wkb_geometry = models.MultiLineStringField(null=False)
+    title = models.TextField(20,null=False)
     powerline = models.TextField(50,null=False)
+    voltage = models.IntegerField(null=False)
+    MONTH_DAY_YEAR = '%m/%d/%Y'
+    MONTH_YEAR = '%m/%Y'
+    DATE_CHOICE= (
+                  (MONTH_DAY_YEAR, 'Month Day Year'),
+                  (MONTH_YEAR, 'Month Year')
+                 )
+    service_date = models.CharField('Date Choice', choices=DATE_CHOICE,  max_length=10, null=False)
+
 
 class testdata_Model(models.Model):
     ogc_fid = models.IntegerField(primary_key=True)
-    wkb_geometry = models.PointField()
-    measurement = models.TextField(10,null=False)
-    value = models.DecimalField(max_digits=3, decimal_places=2)
-
+    wkb_geometry = models.PointField(null=False)
+    device_id = models.TextField(10,null=False)
+    timestamp = models.DateTimeField(null=False)
+    job_id = models.TextField(10,null=False)
+    concentrat = models.IntegerField(null=False)
+    chem_id = models.TextField(10,null=False)
+    amb_temp = models.DecimalField(max_digits=5, decimal_places=2, null=False)
+    rel_humid = models.DecimalField(max_digits=5, decimal_places=2, null=False)
+    precip = models.IntegerField(null=False)
+    air_pressu = models.IntegerField(null=False)
+    wind_speed = models.IntegerField(null=False)
+    wind_direc = models.IntegerField(null=False)
--- lassares_be_app/serializers.py
+++ lassares_be_app/serializers.py
@@ -8,12 +8,13 @@ class fdr_18001_0_11_Serializer(GeoFeatureModelSerializer):
         model = fdr_18001_0_11_Model
         geo_field = 'wkb_geometry'
         id_field='ogc_fid'
-        fields= ('ogc_fid', 'powerline')
+        fields= ('ogc_fid', 'title', 'powerline', 'voltage', 'service_date')
 
 class testdata_Serializer(GeoFeatureModelSerializer):
     class Meta:
         model = testdata_Model
         geo_field = 'wkb_geometry'
         id_field='ogc_fid'
-        fields= ('ogc_fid', 'measurement', 'value')
+        fields= ('ogc_fid', 'device_id', 'timestamp', 'job_id', 'concentrat', 'chem_id', 
+                 'amb_temp', 'rel_humid', 'precip', 'air_pressu', 'wind_speed', 'wind_direc')
 
